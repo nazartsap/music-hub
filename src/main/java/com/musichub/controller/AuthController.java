432a2fb83bc7b61dd95c8da77e98d9ba843315c8
Nazar <nazar.tsap2349@gmail.com>
2025-11-10 21:20:59 +0200

    add api for registration (demo not done yet)

---
+++ src/main/java/com/musichub/controller/AuthController.java
@@ -0,0 +1,19 @@
+package com.musichub.controller;
+
+import com.musichub.model.User;
+import com.musichub.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
